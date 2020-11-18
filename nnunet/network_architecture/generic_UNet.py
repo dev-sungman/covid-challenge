4c8c897f1853f5a8b19dc6ce6a355bd20d036500
rlatjcj <rlatjcj@naver.com>
2020-11-18 10:38:31 +0900

    fix: edit little typo

--- nnunet/network_architecture/generic_UNet.py
+++ nnunet/network_architecture/generic_UNet.py
@@ -28,13 +28,12 @@ from nnunet.network_architecture.non_local import  NONLocalBlock3D
 class Conv2d(nn.Conv2d):
     def __init__(self, in_channels, output_channels, kernel_size, stride=1,
                     padding=0, dilation=1, groups=1, bias=True):
-        super(Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding,
+        super(Conv2d, self).__init__(in_channels, output_channels, kernel_size, stride, padding,
                                     dilation, groups, bias)
     
     def forward(self, x):
         weight = self.weight
-        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2, keepdim=True).mean(
-                            dim3, keepdim=True)
+        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
         weight = weight - weight_mean
         std = weight.view(weight.size(0), -1).std(dim=1).view(-1,1,1,1) + 1e-5
         weight = weight / std.expand_as(weight)
