e942f1224332ae4aeb100bab77245a2229678df4
Sungman-Cho <pccho9108@gmail.com>
2020-11-20 16:33:56 +0900

    update environment_setup.sh: add python environment path

--- environment_setup.sh
+++ environment_setup.sh
@@ -3,3 +3,4 @@ DIR="$( cd "$( dirname -- $0 )" && pwd -P )"
 export nnUNet_raw_data_base=$DIR'/nnUNet_raw_data_base'
 export nnUNet_preprocessed=$DIR'/nnUNet_preprocessed'
 export RESULTS_FOLDER=$DIR'/nnUNet_trained_models'
+export PYTHONPATH='{PYTHONPATH}:'$DIR
